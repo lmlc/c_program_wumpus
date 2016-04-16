315a679c2348edcd4a0ad7e5d284aac5d5221424
shihaoli001 <1242918752@qq.com>
2016-04-16 19:04:30 +0800

    Added files via upload

---
+++ menu.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<conio.h>
+void menu()
+{
+    printf("1.开始游戏：\n 2.排行榜：\n 3.帮助\n");
+    char ch=_getch();
+    switch(ch)
+    {
+
+        case '1':start();break;
+        case '2':paihangbang_r();break;
+        case '3':help();break;
+    }
+}
+void help()
+{
+
+    printf("主角一开始位于随机的小方块\n");
+    printf("每个小方格可能是空的，也可能有陷阱、怪兽或金币\n");
+    printf("陷阱所在的方格和上、下、左、右四个相邻的方格内有微风\n");
+    printf("怪兽所在的方格和上、下、左、右四个相邻的方格内有异味\n");
+    printf("怪兽不会移动\n");
+    printf("当主角位于某个空格时，获得金币或者闻到气味或者感受到微风\n");
+    printf("主角身上带有一定数量的金币，主角每移动一步都需要消耗1金币\n");
+    printf("主角前进时，如果前方是墙壁，则这个动作失败，但获得动作失败的通知并消耗一次动作\n");
+    printf("主角可以向前方射箭，每次射箭都需要消耗10金币\n");
+    printf("主角射箭时，前方最近的一个怪兽被射死，怪兽会发出惨叫声；如果前进方向上没有怪兽，主角可以听到箭头撞击墙壁发出的声音\n");
+    printf("主角如果进入有怪兽或陷阱的方格，主角立即死亡并结算游戏分数\n");
+    printf("游戏的目的是获得地图上的所有金币，或在主角死亡前获得尽可能多的金币\n");
+
+}
+void start(int m_map[100][100],struct MAN *man)
+{
+    printf("选择模式：1.简单；2.中等；3.难；4.自定义");
+    char ch =_getch();
+    switch(ch)
+    {
+
+        case '1' : map(m_map,1,0,0,0);break;
+        case '2':  map(m_map,2,0,0,0);break;
+        case '3':  map(m_map,3,0,0,0):break;
+        case '4': int i,j,k;printf("输入怪物数量，陷阱数量和金币数量：\n");
+                    scanf("%d%d%d",&i,&j,&k);
+                    getchar();
+                    map(m_map,4,i,j,k);break;
+    }
+     if(move(m_map,man)==1)
+     {printf("您赢了\n");
+     printf("请输入您的名字\n");
+     char num[20];
+     scanf("%s",num);
+     paihangbang_w(man->moneycount,num);}
+     else printf("您输了\n")；
+    return ;
+
+}
---
+++ paihangbang.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include"head.h"
+#include<string.h>
+void paihangbang_r()
+{
+
+    FILE *fp=fopen("paihangbang.txt","r+");
+    struct PERSON{char a[20];int money;}person[10];
+    for(i=0;i<10;i++)
+    {
+
+        fscanf(fp,"%s%d",person[i].a,&person[i].money);
+    }
+    fclose(fp);
+    for(i=0;i<10;i++)
+    {
+
+        printf("%s %d\n",person[i].a,person[i].money);
+    }
+}
+
+void paihangbang_w(int money, char name[])
+{
+    FILE *fp=fopen("paihangbang.txt","r");
+    struct PERSON{char [20];int money;}person[200];
+    int i=0;
+    while(!feof(fp))
+    {
+      fscanf(fp,"%s%d",person[i].a,&person[i].money);
+      i++;
+    }
+    int total = i;
+    fclose(fp);
+    for(i=0;i<total;i++)
+    {
+
+        if(person[i].money<=money) break;
+    }
+    int j;
+    for(j=total;j>i;j--)
+    {
+
+        person[j]=person[j-1];
+    }
+    strcpy(person[i].a,num);
+    person[i].money=money;
+    printf("ÄúµÄÅÅÃûÎª%d"£¬i+1);
+    FILE *fp1=fopen("paihangbang.txt","w");
+    for(i=0;i<=total;i++)
+    {
+
+        fprintf(fp1,"%s %d",person[i].a,person[i].money);
+    }
+    fclose(fp1);
+
+
+}
+
