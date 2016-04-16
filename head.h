1221b1aa2baae286eeb944c8cf36cda6c2383a28
Qinglang <treeos@163.com>
2016-04-16 12:53:13 +0800

    Added files via upload
    the head file

---
+++ head.h
@@ -0,0 +1,13 @@
+struct MAN{
+    int x;
+    int y;
+    int moneycount;
+    int lifestyle;//(2:die without money) (1:killed by monster)(0:lived)
+};
+#ifndef
+#define UNROAD 0;
+#define BEENROAD 1;
+#define MONEY 3;
+#define WALL 4;
+#define TRAP 5;
+#endif
