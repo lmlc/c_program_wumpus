a7f37cb3a83725a1ed9fd2799830ca73eeb1018e
Qinglang <treeos@163.com>
2016-04-16 12:55:08 +0800

    Added files via upload
    try to have a change

--- head.h
+++ head.h
@@ -5,7 +5,7 @@ struct MAN{
     int lifestyle;//(2:die without money) (1:killed by monster)(0:lived)
 };
 #ifndef
-#define UNROAD 0;
+#define UNROAD 0;//try to change
 #define BEENROAD 1;
 #define MONEY 3;
 #define WALL 4;
