f9c3acf829fca9dc0e906276417297f7fc3ab0f7
lg998 <906469565@qq.com>
2016-04-16 15:57:52 +0800

    Update head.h

--- head.h
+++ head.h
@@ -7,6 +7,7 @@ struct MAN{
     int y;
     int moneycount;
     int lifestyle;//(3:killed by thorn)(2:die without money) (1:killed by monster)(0:lived)
+    int direction;//(0:up)(1:right)(2:down)(3:left)
 };
 
 #define UNROAD 0;
