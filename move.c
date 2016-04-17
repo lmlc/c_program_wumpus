1fda3faa9f8c2b7cfa933c8d191bb5c826d5abb3
lg998 <906469565@qq.com>
2016-04-17 21:35:54 +0800

    Update move.c

--- move.c
+++ move.c
@@ -1,11 +1,11 @@
 #include <head.h>
 
-int move(int map[1000][1000], int a, int b, struct MAN player)   //返回1代表赢了，返回0代表输了
+int move(int map[1000][1000], struct MAN *player)   //返回1代表赢了，返回0代表输了
 {
         char ch;
         int winorlose = 0;
-        printmap (&player, map);
-        tip_move(&player, map);
+        printmap (player, map);
+        tip_move(player, map);
         while (!kbhit())           //判断键盘有无输入
         {
                 ch=getch();
@@ -13,43 +13,43 @@ int move(int map[1000][1000], int a, int b, struct MAN player)   //返回1代表
                 {
                         player->faceto=3;         //改变方向
                         player->moneycount-=1;    //扣钱
-                        printmap (&player, map);  //打印地图
-                        tip_move(&player, map);   //移动的提示
+                        printmap (player, map);  //打印地图
+                        tip_move(player, map);   //移动的提示
                 }
                 else if (ch=='a')
                 {
                         player->faceto=0;
                         player->moneycount-=1;
-                        printmap (&player, map);
-                        tip_move(&player, map);
+                        printmap (player, map);
+                        tip_move(player, map);
                 }
                 else if (ch=='s')
                 {
                         play->faceto=1;
                         player->moneycount-=1;
-                        printmap (&player, map);
-                        tip_move(&player, map);
+                        printmap (player, map);
+                        tip_move(player, map);
                 }
                 else if (ch=='s')
                 {
                         player->faceto=2;
                         player->moneycount-=1;
-                        printmap (&player, map);
-                        tip_move(&player, map);
+                        printmap (player, map);
+                        tip_move(player, map);
                 }
                 else if (ch=='d')
                 {
                         player->faceto=3;
                         player->moneycount-=1;
-                        printmap (&player, map);
-                        tip_move(&player, map);
+                        printmap (player, map);
+                        tip_move(player, map);
                 }
                 else if (ch==' ')                  //射箭
                 {
                         if (player->moneycount>=10)
                         {
                                 player->moneycount-=10;
-                                if (tip_shoot(&player, map)==1)    //tip_shoot函数返回了1代表怪物死了
+                                if (tip_shoot(player, map)==1)    //tip_shoot函数返回了1代表怪物死了
                                         return 1;
                         }
                         else
@@ -76,7 +76,7 @@ int move(int map[1000][1000], int a, int b, struct MAN player)   //返回1代表
                                 player->x--;
                         }
                         player->moneycount-=1;
-                        tip_move(&player, map);
+                        tip_move(player, map);
                         map[player->x][player->y]==BEENLOAD;    //走过的路
                         if (player->lifestyle!=0)
                         {
